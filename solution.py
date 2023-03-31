e3a0f448ce51c43eee7a8b6cb345dc09b412cb06
squrem <108416846+squrem@users.noreply.github.com>
2023-03-31 15:39:56 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import numpy as np
 
-
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+# task 1
+chat_id = 434559054 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
     # Измените код этой функции
