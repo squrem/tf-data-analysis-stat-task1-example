d4725536ab86d3b73552d3b60a818e2b9b0589fd
squrem <108416846+squrem@users.noreply.github.com>
2023-04-05 10:58:25 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -8,4 +8,9 @@ def solution(x: np.array) -> float:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    N = 587
+    y = np.log(x - N) 
+    mu = y.mean()
+    s = y.std(ddof=1)
+    return mu - 0.5 * np.log(np.exp(s**2 /len(x) - 2 * mu) + 1)
+    
